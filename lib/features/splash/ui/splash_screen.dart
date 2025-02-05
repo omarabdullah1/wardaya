39123c1eae8322c6ed3d30e1a618863567c22f33
Omar Abdullah <omarabdulla1>
2025-02-05 04:42:58 +0200

    - Added Native Splash Screen for android above - under 21 - Added Native Splash Screen for ios - Added Animated Splash screen with logo animation

---
+++ lib/features/splash/ui/splash_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animated_splash/flutter_animated_splash.dart'
+    as animated_splash;
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wardaya/core/theming/colors.dart';
+import '../../../core/di/dependency_injection.dart';
+import '../../authentication/logic/cubit/login_cubit.dart';
+import '../../authentication/ui/login_screen.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return animated_splash.AnimatedSplash(
+      type: animated_splash.Transition.scale,
+      navigator: BlocProvider(
+        create: (context) => getIt<LoginCubit>(),
+        child: const SignInScreen(),
+      ),
+      curve: Curves.elasticInOut,
+      durationInSeconds: 15,
+      backgroundColor: ColorsManager.mainRose,
+      child: Image.asset(
+        'assets/images/logo/Wardaya_Application_splash.gif',
+      ),
+    );
+  }
+}
