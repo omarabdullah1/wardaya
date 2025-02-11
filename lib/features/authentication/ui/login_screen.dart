498fc88d598ffef65efea87fc85efa07d74ac971
Omar Abdullah <omarabdulla1>
2025-02-11 19:58:35 +0200

    - Updated the discover ideas section with static images - Updated the home screen with the homeStartTheYearWithTulips section padding - Updated the width and height of the category progress scroll widget item

--- lib/features/authentication/ui/login_screen.dart
+++ lib/features/authentication/ui/login_screen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:wardaya/core/blocs/general/cubit/general_cubit.dart';
 import 'package:wardaya/core/helpers/extensions.dart';
 import 'package:wardaya/core/routing/routes.dart';
 import 'package:localization/localization.dart';
